6925e57812708008929bce4c009fd4d55473f975
Anish Patel <anishpatel39@gmail.com>
2019-10-25 10:09:41 -0400

    Final Commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,149 +1,109 @@
 $(document).ready(function() 
 {
-	var rand = [];
 
-	for (var r = 19; r < 121; r++) 
+	  //Variables
+	  var winCount = 0; 
+	  var lossCount = 0; 
+	  var userScore = 0; 
+	  var computerScore = Math.floor(Math.random() * 102) + 19; 
+	  var first = Math.floor(Math.random() * 12) + 1; 
+	  var second = Math.floor(Math.random() * 12) + 1; 
+	  var third = Math.floor(Math.random() * 12) + 1; 
+	  var fourth = Math.floor(Math.random() * 12) + 1; 
+
+	  //Displays text score
+	  $("#winnerScore").text("Wins: " + winCount);
+	  $("#loserScore").text("Losses: " + lossCount);
+	  $("#scoreLimit").text("Game Score: " + computerScore);
+	  $("#playerScore").text("User Score: " + userScore);
+
+  
+	// Reset Function
+	function resetGame() 
 	{
-		rand.push(r);
+	  userScore = 0;
+	  computerScore = Math.floor(Math.random() * 102) + 19; 
+	  first = Math.floor(Math.random() * 12) + 1; 
+	  second = Math.floor(Math.random() * 12) + 1; 
+	  third = Math.floor(Math.random() * 12) + 1; 
+	  fourth = Math.floor(Math.random() * 12) + 1; 
+	  $("#scoreLimit").text("Game Score: " + computerScore);
+	  $("#playerScore").text("User Score: " + userScore);
+
 	}
-
-	var crystals = [];
-
-	for (var c = 1; c < 13; c++) 
+  
+// Click Function
+	function winGame() 
 	{
-
-		crystals.push(c);
+	  alert("Winner Winner!");
+	  winCount = winCount + 1;
+	  $("#winnerScore").text("Wins: " + winCount);
+	  resetGame();
 	}
-
-	var randNumber; 
-	var crystalNumbers = []; 
-
-	var c1;
-	var c2;
-	var c3;
-	var c4;
-
-  var totalScore = 0; 
-
-	var wins = 0;
-	var losses = 0;
-
-
-	function pickRandomNumber(arr) 
+  
+// Click Function
+	function loseGame() 
 	{
-
-		var x = arr[Math.floor(Math.random() * arr.length)];
-		randNumber = x;
-		$("#randomNumber").html(randNumber);
-
-		console.log("random number: " + randNumber);
-
-	} 
-
-	function pickRandomCrystals(arr) 
+	  alert("OOOO... So Close!");
+	  lossCount = lossCount + 1;
+	  $("#loserScore").text("Losses: " + lossCount);
+	  resetGame();
+	}
+  
+	// Click Function
+	$("#crystal1").click(function() 
 	{
-
-		for (var y = 0; y < 4; y++)
+	  userScore = userScore + first; 
+	  $("#playerScore").text("User Score: " + userScore);
+		if (userScore === computerScore) 
 		{
-
-			var a = arr[Math.floor(Math.random() * arr.length)];
-
-			crystalNumbers.push(a);
-		}
-    
-		console.log("crystal numbers: " + crystalNumbers);
-
-	} 
-
-	function crystalValues(arr) {
-
-		for (i = 0; i < arr.length; i++) 
+		  winGame();
+		} else if (userScore > computerScore) 
 		{
-
-		$("#button-" + (i+1)).attr("value", arr[i]);
-		console.log(this);
-		}
-
-		c1 = arr[0];
-		c2 = arr[1];
-		c3 = arr[2];
-		c4 = arr[3];
-	} 
-
-	function gameReset(x) 
-	{
-
-		crystalNumbers = []; 
-
-		pickRandomNumber(rand);
-
-		pickRandomCrystals(crystals);
-
-		crystalValues(crystalNumbers);
-
-		totalScore = 0;
-		$("#totalNumber").html(totalScore);
-
-		alert(x);
-	} 
-
-	pickRandomNumber(rand); 
-	pickRandomCrystals(crystals); 
-	crystalValues(crystalNumbers);
-
-
-
-		$("#button-1").on("click", function() 
-		{
-
-			totalScore += c1;
-			$("#totalNumber").html(totalScore);
-		});
-
-		$("#button-2").on("click", function() 
-		{
-
-			totalScore += c2;
-			$("#totalNumber").html(totalScore);
-		});
-
-		$("#button-3").on("click", function() 
-		{
-
-			totalScore += c3;
-			$("#totalNumber").html(totalScore);
-		});
-
-		$("#button-4").on("click", function() 
-		{
-
-			totalScore += c4;
-			$("#totalNumber").html(totalScore);
-		});
-
-	$("button").on("click", function() 
-	{
-		if (totalScore == randNumber) 
-		{
-
-			wins++;
-			console.log(totalScore);
-			$("#totalNumber").html(totalScore);
-			$("#wins").html("Wins: " + wins);
-
-
-			setTimeout(function() {gameReset("WINNER WINNER CHICKEN DINNER!!")}, 200);
-		}
-
-		else if (totalScore > randNumber)
-		{
-
-			losses++;
-			$("#totalNumber").html(totalScore);
-			$("#losses").html("Losses: " + losses);
-
-			setTimeout(function() {gameReset("FATALITY!")}, 200);
+		  loseGame();
 		}
 	});
-
-}); 
+  
+	// Click Function
+	$("#crystal2").click(function() 
+	{
+	  userScore = userScore + second; 
+		$("#playerScore").text("User Score: " + userScore);
+		if (userScore === computerScore) 
+		{
+		  winGame();
+		} else if (userScore > computerScore) 
+		{
+		  loseGame();
+		}
+	});
+  
+	// Click Function
+	$("#crystal3").click(function() 
+	{
+	  userScore = userScore + third; 
+		$("#playerScore").text("User Score: " + userScore);
+		if (userScore === computerScore) 
+		{
+		  winGame();
+		} else if (userScore > computerScore) 
+		{
+		  loseGame();
+		}
+	});
+  
+	// Click Function
+	$("#crystal4").click(function() 
+	{
+	  userScore = userScore + fourth; 
+	  $("#playerScore").text("User Score: " + userScore);
+		if (userScore === computerScore) 
+		{
+		  winGame();
+		} else if (userScore > computerScore) 
+		{
+		  loseGame();
+		}
+	});
+  
+  });
